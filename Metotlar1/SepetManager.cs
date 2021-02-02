e921be6a832952836517a3df8b84ea41e4d88094
Mustafa Kılıç <musty48@msn.com>
2021-02-02 11:51:04 +0300

    Add project files.

---
+++ Metotlar1/SepetManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar1
+{
+    class SepetManager
+    {
+        //naming convention
+
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat)
+        {
+            Console.WriteLine("Tebrikle. Sepete eklendi : "+ urunAdi);
+        }
+
+    }
+}
